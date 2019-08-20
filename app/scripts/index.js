68db814e4de0f57b25ec00c53839dcf10d518883
DimaOsinnii <ozerland@gmail.com>
2019-08-20 16:47:14 +0300

    Created gulp file and project setup done

---
+++ app/scripts/index.js
@@ -0,0 +1,3 @@
+let  dimaaaa = 25;
+console.log(dimaaaa);
+console.log(dimaaaa + 10);
---
+++ build/js/index.js
@@ -0,0 +1,2 @@
+var dimaaaa=25;console.log(dimaaaa),console.log(dimaaaa+10);
+//# sourceMappingURL=index.js.map
